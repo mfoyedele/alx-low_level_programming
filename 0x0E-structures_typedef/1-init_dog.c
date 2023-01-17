75585ba003443a96932835e1c28ebdcfaa6329ee
Musa Oyedele <kehindet@gmail.com>
2023-01-16 18:57:50 -0800

    Structures typedef created

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initializes a variable of type struct dog.
+ * @d: The dog to be initialized.
+ * @name: The dog name.
+ * @age: The dog age.
+ * @owner: The dog owner.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+# Struc def
